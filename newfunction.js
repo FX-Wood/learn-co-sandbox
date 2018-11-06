8d8e3d3234076d44570eca887de9e14a5ef13964
FX Wood <fxwood2@gmail.com>
2018-11-06 05:45:20 +0000

    automatically backed up by learn

---
+++ newfunction.js
@@ -0,0 +1,30 @@
+function sayHello()  {
+  console.log('Hello!')
+}
+
+function sayHelloToIsabel()  {
+  console.log('Hello, Isabel!')
+}
+
+function sayHelloToSofia()  {
+  console.log('Hello, Sofia!')
+}
+
+function sayHelloToBrendan()  {
+  console.log('Hello, Brendan!')
+}
+
+
+sayHello();
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+
+function sayHelloTo(firstName)  {
+  console.log(`Hello, ${firstName}!`)
+}
+
+sayHelloTo('Isabel')
+sayHelloTo('Jane')
+sayHelloTo('R2-D2')
+sayHelloTo(1)
